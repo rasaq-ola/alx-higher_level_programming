595298c0cb7f7872c1a67634731b15e12d016a44
rasaq-ola <rasaqola196@gmail.com>
2024-03-07 05:16:14 -0800

    input_output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+"""Defines a function that reads a text file"""
+
+def read_file(filename=""):
+    """printing the content of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
