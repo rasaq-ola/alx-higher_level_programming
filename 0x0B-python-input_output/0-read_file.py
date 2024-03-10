d2b4e1941aa79f6aee7e3274daca358475f9f63f
rasaq-ola <rasaqola196@gmail.com>
2024-03-10 14:51:36 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines a function that reads a text file"""
-
 def read_file(filename=""):
     """printing the content of a UTF8 text file"""
     with open(filename, encoding="utf-8") as file:
