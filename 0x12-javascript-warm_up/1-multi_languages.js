bd9774dbadfdef54da9e2700454bd2dae23ed620
rasaq-ola <rasaqola196@gmail.com>
2024-03-12 11:58:57 +0100

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,8 +2,10 @@
 /* A script that prints 3 lines of code, with strings:
  * "C is fun", "Python is cool", "JavaScript is amazing".
  */
-const my_C-code = "C is fun \n"
-const my_Python-code = "Python is cool \n"
-const my_JS-code = "JavaScript is amazing \n"
+const my_C-code = "C is fun \n";
+const my_Python-code = "Python is cool \n";
+const my_JS-code = "JavaScript is amazing \n";
 
-console.log(my_C-code, my_python-code, my_JS-code)
+console.log(my_C-code);
+console.log(my_Python-code);
+console.log(my_JS-code);
