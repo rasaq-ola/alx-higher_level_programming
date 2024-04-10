d75953d39032365c9fadc5390f98fcf18bd7fead
rasaq-ola <115763421+rasaq-ola@users.noreply.github.com>
2024-04-10 12:57:42 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/13-main.js
+++
@@ -1,6 +0,0 @@
-/* In 13-main.js
-*/
-#!/usr/bin/node
-
-const add = require('./13-add').add;
-console.log(add(3, 5));
--- 0x12-javascript-warm_up/17-args.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-console.log(process.argv);
-console.log(`length of argv is: ${process.argv.length}`);
