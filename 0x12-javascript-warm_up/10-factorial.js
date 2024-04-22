459ba9d5966e67a04c20c9c1ede8a89d392bbc1d
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-04-22 20:21:13 +0100

    warm-up_JavaScript

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function factorial(num) {
+  if (isNaN(num)) return 1;
+  num = parseInt(num);
+  if (num === 0 || num === 1) return 1;
+  return num * factorial(num - 1);
+}
+console.log(factorial(process.argv[2]));
