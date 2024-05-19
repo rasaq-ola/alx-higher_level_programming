83f24b9eeb40d7c0b39389698a25e0a8ef894bc5
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-05-20 00:03:10 +0100

    python network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,7 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))  
+        print("\t- utf8 content: {}".format(
+            body.decode('utf-8')
+        ))                                         
+~                                    
