1c37dc16efeea54ca3b16ad952431fbdab2ae0d1
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-05-19 01:18:32 +0100

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 Module to find a peak in a list of unsorted integers.
 """
 
+
 def find_peak(list_of_integers):
     """
     Finds a peak in a list of unsorted integers.
@@ -20,7 +21,8 @@ def find_peak(list_of_integers):
     mid = n // 2
     mid_val = list_of_integers[mid]
 
-    if (mid == 0 or mid_val >= list_of_integers[mid - 1]) and (mid == n - 1 or mid_val >= list_of_integers[mid + 1]):
+    if (mid == 0 or mid_val >= list_of_integers[mid - 1]) and (mid == n - 1 or
+       mid_val >= list_of_integers[mid + 1]):
         return mid_val
     elif mid > 0 and list_of_integers[mid - 1] > mid_val:
         return find_peak(list_of_integers[:mid])
