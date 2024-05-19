f80dea9d9ccd0822ec012359c62d2ea0e41dd385
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-05-19 01:00:57 +0100

    python network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Display all HTTP methods the server will accept
 curl -sI "$1" | grep "Allow:" | cut -d " " -f2-
-#!/bin/bash
-# Display all HTTP methods the server will accept
-curl -sI "$1" | grep "Allow:" | cut -d " " -f2-
