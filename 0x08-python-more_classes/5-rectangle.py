35983ae3f9a4ca8fbb4f3fd23364bee7df655bdf
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-06 16:09:28 +0100

    classes and object

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -79,4 +79,24 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        return rect_1 if rect_1.area
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+
+
+if __name__ == "__main__":
+    # Example usage
+    my_rectangle = Rectangle(2, 4)
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+    # Deleting the Rectangle instance
+    del my_rectangle
+
+    try:
+        # Attempting to set an invalid width
+        my_rectangle = Rectangle(2, 4)
+        my_rectangle.width = 12
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    # Creating multiple Rectangle instances
+    for i in range(10):
+        m1 = Rectangle(2, 4)
