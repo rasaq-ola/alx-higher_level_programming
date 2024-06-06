95e35a3eacf4343ba6832b9b29f0d8131b623a2f
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-06 14:13:31 +0100

    unitest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 This module defines a function to add two integers.
 """
 
+
 def add_integer(a, b=98):
     """
     Adds two integers or floats casted to integers.
@@ -13,6 +14,7 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: If either `a` or `b` is not an integer or float.
+        ValueError: If either `a` or `b` is a NaN or infinite.
 
     Returns:
         int: The sum of `a` and `b`, casted to integers.
@@ -21,5 +23,10 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    if isinstance(a, float) and (a != a or a in (float('inf'), -float('inf'))):
+        raise ValueError("cannot convert float NaN or infinite to integer")
+    if isinstance(b, float) and (b != b or b in (float('inf'), -float('inf'))):
+        raise ValueError("cannot convert float NaN or infinite to integer")
 
     return int(a) + int(b)
