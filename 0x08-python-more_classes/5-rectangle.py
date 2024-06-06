51afe3b396b58a6fd07a2e76591b46006d5db68f
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-06 16:13:00 +0100

    classes and object

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-
-"""
-This module defines a Rectangle class.
-"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """
-    Defines a rectangle with width and height attributes.
-    """
-
-    number_of_instances = 0
-    print_symbol = '#'
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initializes a Rectangle instance with optional width and height."""
+        """Initialize a new rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """Retrieve the width of the Rectangle."""
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -31,12 +26,11 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Retrieve the height of the Rectangle."""
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -46,57 +40,28 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
-        """Calculate the area of the Rectangle."""
+        """Return the area of the Rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculate the perimeter of the Rectangle."""
+        """Return the perimeter of the Rectangle."""
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation of the Rectangle."""
+        """Return the printable representation of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return ''
-        return '\n'.join([str(self.print_symbol) * self.__width] * self.__height)
+            return ""
+        return "\n".join(['#' * self.__width] * self.__height)
 
     def __repr__(self):
-        """Return a string representation of the Rectangle for recreation."""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        """Return the string representation of the Rectangle."""
+        return "Rectangle({}, {})".format(
+            self.__width, self.__height
+        )
 
     def __del__(self):
         """Print a message when a Rectangle instance is deleted."""
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Return the rectangle with the larger area."""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
-
-
-if __name__ == "__main__":
-    # Example usage
-    my_rectangle = Rectangle(2, 4)
-    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-    # Deleting the Rectangle instance
-    del my_rectangle
-
-    try:
-        # Attempting to set an invalid width
-        my_rectangle = Rectangle(2, 4)
-        my_rectangle.width = 12
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    # Creating multiple Rectangle instances
-    for i in range(10):
-        m1 = Rectangle(2, 4)
