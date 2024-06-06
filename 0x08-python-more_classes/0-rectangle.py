0011692b328f42812dc8e0239729dc975d93aa31
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-06 17:01:21 +0100

    classes and object

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+"""Defines a simple Rectangle class."""
+
+
 class Rectangle:
-    """An empty class that defines a rectangle."""
-    pass
+    """Represent a simple rectangle."""
+
+    def __init__(self):
+        """Initialize a new rectangle."""
+        pass
+
+    @property
+    def dict_(self):
+        """Return an empty dictionary."""
+        return {}
+
+if __name__ == "__main__":
+    myrectangle = Rectangle()
+    print(type(myrectangle))
+    print(myrectangle.dict_)
